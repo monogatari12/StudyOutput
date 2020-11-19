a6fb57f718ea78531ad497d118ffccf8f27e6ce5
monogatari12 <74235230+monogatari12@users.noreply.github.com>
2020-11-19 12:21:50 +0900

    仕様変更に伴う追加。

--- StudyOutput/src/main/java/com/example/demo/MainController.java
+++ StudyOutput/src/main/java/com/example/demo/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
 
 
 @Controller
@@ -18,5 +20,21 @@ public ModelAndView index(ModelAndView mv) {
 	mv.setViewName("index");
 	return mv;
 }
-
+	@RequestMapping(value="/main",method=RequestMethod.GET)
+	public ModelAndView main(ModelAndView mv) {
+		mv.setViewName("main");
+		return mv;
 }
+	@RequestMapping(value="/edit",method=RequestMethod.GET)
+	public ModelAndView edit(ModelAndView mv) {
+		mv.setViewName("edit");
+		return mv;
+	}
+	@RequestMapping(value="/edit")
+	public String edit(Model model) {
+		model.addAttribute("day","11/14");
+		return "edit";
+	}
+	}
+
+
